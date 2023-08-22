9d62671ff7cebe24ec6743eecd42bf819b5d7faf
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-22 17:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let boldText = document.querySelectorAll("strong");
+
 function highlight() {
     //Write your code here
-
-
+ for(let i = 0;i<boldText.length;i++){
+            boldText[i].style.color = "green";
+        }
 }
 
 
 function return_normal() {
     //Write your code here
-
+	for(let i = 0;i<boldText.length;i++){
+            boldText[i].style.color = "black";
+        }
     
 }
